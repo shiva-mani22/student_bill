d6260596c1ccaf4c9861ff217412c1ab04caa3cf
Uchiha <vshivamani3369@gmail.com>
2025-06-19 13:54:42 +0530

    added responsive design

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,35 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiRename } from "react-icons/bi";
 import { FaRegUserCircle } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { IoRepeat } from "react-icons/io5";
+
 const Register = () => {
+  const [formData,setFormData]=useState({
+  name:'',
+  userName:'',
+  email:'',
+  password:''
+});
+let handelChange=(e)=>{
+  let {name,value}=e.target
+  setFormData((prev)=>({...prev,[name]:value}))
+}
+let handelSubmit=(e)=>{
+  e.preventDefault()
+  console.log(formData);
+  
+}
   return (
-    <div className='flex items-center justify-center m-5 bg-gray-300 size-full rounded-md'>
-      <form action="" className='bg-white flex flex-col justify-center px-10 items-center w-1/2 h-120 gap-6 drop-shadow-lg'>
-        <div className='w-full flex items-center justify-center font-bold '>
-          <h1>Registration Form</h1>
+    <div className='flex items-center justify-center bg-gray-300 size-full'>
+      <form action="" className='bg-white flex flex-col justify-center px-10 items-center w-1/2 h-3/4 gap-8 rounded-lg 
+      drop-shadow-lg max-sm:w-[90%]' onSubmit={handelSubmit}>
+        <div className='w-full flex items-center justify-center font-bold  '>
+          <h1 className='text-3xl max-sm:text-[25px]'>Registration Form</h1>
           </div>
         <div className='border-2 w-full flex items-center justify-center rounded-sm'>
-          <input type="text" placeholder='Enter Name' className='outline-none px-3 w-full'/>
-          <span><BiRename /></span>
+          <input type="text" placeholder='Enter Name' name='name' className='outline-none px-3 w-full'  onChange={handelChange}/>
+          <span className='pr-1.5'><BiRename /></span>
         </div>
         <div className='border-2 w-full flex items-center justify-center rounded-sm'>
-          <input type="text" placeholder='Enter User Name' className='outline-none px-3 w-full'/>
-          <span><FaRegUserCircle /></span>
+          <input type="text" placeholder='Enter User Name' name='userName' className='outline-none px-3 w-full' onChange={handelChange}/>
+          <span className='pr-1.5'><FaRegUserCircle /></span>
         </div>
         <div className='border-2 w-full flex items-center justify-center rounded-sm'>
-          <input type="email" placeholder='Enter Email' className='outline-none px-3 w-full'/>
-          <span><MdEmail /></span>
+          <input type="email" placeholder='Enter Email' name='email' className='outline-none px-3 w-full'  onChange={handelChange}/>
+          <span className='pr-1.5'><MdEmail /></span>
         </div>
         <div className='border-2 w-full flex items-center justify-center rounded-sm'>
-          <input type="password" placeholder='Enter Password' className='outline-none px-3 w-full'/>
-          <span><RiLockPasswordFill /></span>
+          <input type="password" placeholder='Enter Password' name='password' className='outline-none px-3 w-full' onChange={handelChange}/>
+          <span className='pr-1.5'><RiLockPasswordFill /></span>
         </div>
         <div className='border-2 w-full flex items-center justify-center rounded-sm'>
           <input type="password" placeholder='Re-Enter Password' className='outline-none px-3 w-full'/>
-          <span><IoRepeat /></span>
+          <span className='pr-1.5'><IoRepeat /></span>
         </div>
         <div className='border-2 w-full flex items-center justify-center bg-black'>
           <button className='text-white w-full'>Click</button>
